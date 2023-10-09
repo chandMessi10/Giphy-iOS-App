a9ead9751a933b29d8c413e4f859c9635fb52918
chandMessi10 <surajchand3010@gmail.com>
2023-10-09 12:40:24 +0545

    Initial Commit

---
+++ Giphy iOS App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Giphy iOS App
+//
+//  Created by Suraj Chand on 09/10/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Giphy iOS App/Giphy_iOS_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Giphy_iOS_AppApp.swift
+//  Giphy iOS App
+//
+//  Created by Suraj Chand on 09/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct Giphy_iOS_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
